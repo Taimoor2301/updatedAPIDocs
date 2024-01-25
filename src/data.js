c4dcfcbc28e7604c4386e81168f93c06a99c327d
Taimoor Ali <taimoorali4214@gmail.com>
2024-01-25 14:00:23 +0500

    made code generator functions

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+import {API_BASE_URL} from './utils/constants'
 const javascript = `const Code = function Home() {
 	return (
 		<div className='grid grid-cols-8'>
@@ -45,16 +46,19 @@ const php = `<!DOCTYPE html>
 
 export const data = [
 	{
-		title: "This is Topic 1",
+		title: "Get all properties",
 		id: "#topic1",
 		description:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
-		javascript,
-		php,
-		python,
+			"This API endpoint provides a way to retrieve a list of all properties. Properties may include information about real estate, products, or any other relevant entities depending on the context of the application.",
+		javascript : jsCode('properties'),
+		php : phpCode('properties')
+
+
+					,
+		python :pythonCode('properties'),
 	},
 	{
-		title: "This is Topic 2",
+		title: "Get property details",
 		id: "#topic2",
 		javascript,
 		php,
@@ -63,7 +67,7 @@ export const data = [
 			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
 	},
 	{
-		title: "This is Topic 3",
+		title: "Get listed properties",
 		id: "#topic3",
 		javascript,
 		php,
@@ -72,7 +76,7 @@ export const data = [
 			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
 	},
 	{
-		title: "This is Topic 4",
+		title: "Get listing details",
 		id: "#topic4",
 		javascript,
 		php,
@@ -81,25 +85,25 @@ export const data = [
 			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
 	},
 	{
-		title: "This is Topic 5",
+		title: "Get bought properties",
 		id: "#topic5",
-		description:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
 		javascript,
 		php,
 		python,
+		description:
+			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
 	},
 	{
-		title: "This is Topic 6",
+		title: "Get sold properties",
 		id: "#topic6",
-		description:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
 		javascript,
 		php,
 		python,
+		description:
+			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
 	},
 	{
-		title: "This is Topic 7",
+		title: "Get buyers",
 		id: "#topic7",
 		javascript,
 		php,
@@ -108,7 +112,7 @@ export const data = [
 			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
 	},
 	{
-		title: "This is Topic 8",
+		title: "Get authorized users",
 		id: "#topic8",
 		javascript,
 		php,
@@ -117,7 +121,7 @@ export const data = [
 			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
 	},
 	{
-		title: "This is Topic 9",
+		title: "Get propert versions",
 		id: "#topic9",
 		javascript,
 		php,
@@ -125,85 +129,112 @@ export const data = [
 		description:
 			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
 	},
-	{
-		title: "This is Topic 10",
-		id: "#topic10",
-		javascript,
-		php,
-		python,
-		description:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
-	},
-	{
-		title: "This is Topic 11",
-		id: "#topic11",
-		javascript,
-		php,
-		python,
-		description:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
-	},
-	{
-		title: "This is Topic 12",
-		id: "#topic12",
-		javascript,
-		php,
-		python,
-		description:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
-	},
-	{
-		title: "This is Topic 13",
-		id: "#topic13",
-		javascript,
-		php,
-		python,
-		description:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
-	},
-	{
-		title: "This is Topic 14",
-		id: "#topic14",
-		javascript,
-		php,
-		python,
-		description:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
-	},
-	{
-		title: "This is Topic 15",
-		id: "#topic15",
-		javascript,
-		php,
-		python,
-		description:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
-	},
-	{
-		title: "This is Topic 16",
-		id: "#topic16",
-		javascript,
-		php,
-		python,
-		description:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
-	},
-	{
-		title: "This is Topic 17",
-		id: "#topic17",
-		javascript,
-		php,
-		python,
-		description:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
-	},
-	{
-		title: "This is Topic 18",
-		id: "#topic18",
-		javascript,
-		php,
-		python,
-		description:
-			"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae et ullam dolores veritatis deleniti tenetur inventore sunt temporibus cum debitis similique totam vitae provident, libero nesciunt nostrum numquam, amet eligendi tempora nulla. Numquam, rerum mollitia vitae et porro eius deserunt ut tempore facilis, fugiat minima. Consectetur illo tempore minima necessitatibus.",
-	},
+
 ];
+
+
+function jsCode(endpoint){
+ return `async function fetchData() {
+	try {
+		const response = await fetch('${API_BASE_URL}/${endpoint}', {
+			method: 'GET',
+			headers: {
+				'Authorization': 'Bearer YOUR_API_KEY', // Replace 'YOUR_API_KEY' with the actual API key
+				'Content-Type': 'application/json',
+			}
+		});
+
+		const data = await response.json();
+		console.log(data);
+	} catch (error) {
+		console.error('Error fetching data:', error.message);
+	}
+}`
+}
+
+function phpCode(endpoint){
+	return `<?php
+
+	function fetchData() {
+			// Replace 'YOUR_API_KEY' and 'API_BASE_URL' with the actual API key and base URL
+			$apiBaseUrl = '${API_BASE_URL}';
+			$apiEndpoint = '/${endpoint}';
+			$apiKey = 'YOUR_API_KEY';
+
+			$url = $apiBaseUrl . $apiEndpoint;
+			$headers = array(
+					'Authorization: Bearer ' . $apiKey,
+					'Content-Type: application/json',
+			);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+			try {
+					$response = curl_exec($ch);
+
+					// Check for cURL errors
+					if (curl_errno($ch)) {
+							throw new Exception('cURL error: ' . curl_error($ch));
+					}
+
+					$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+					// Check for HTTP errors
+					if ($httpCode >= 400) {
+							throw new Exception('HTTP error! Status: ' . $httpCode);
+					}
+
+					$data = json_decode($response, true);
+					echo 'Fetched data: ';
+					print_r($data);
+
+					// You can do something with the fetched data here
+
+			} catch (Exception $error) {
+					echo 'Error fetching data: ' . $error->getMessage();
+			} finally {
+					curl_close($ch);
+			}
+	}
+
+	// Call the function to fetch data
+	fetchData();
+
+	?>
+	`
+}
+
+function pythonCode(endpoint){
+	return `import requests
+
+	def fetch_data():
+			# Replace 'YOUR_API_KEY' and 'API_BASE_URL' with the actual API key and base URL
+			api_base_url = '${API_BASE_URL}'
+			api_endpoint = '/${endpoint}'
+			api_key = 'YOUR_API_KEY'
+
+			headers = {
+					'Authorization': f'Bearer {api_key}',
+					'Content-Type': 'application/json',
+			}
+
+			try:
+					response = requests.get(f'{api_base_url}{api_endpoint}', headers=headers)
+
+					# Raise an exception for HTTP errors
+					response.raise_for_status()
+
+					data = response.json()
+					print('Fetched data:', data)
+
+					# You can do something with the fetched data here
+
+			except requests.exceptions.RequestException as error:
+					print('Error fetching data:', error)
+
+	# Call the function to fetch data
+	fetch_data()
+	`
+}
