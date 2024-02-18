468172729422da252fe0e0c717bcfe2dcfa87c99
baibars313 <baibarsrajput@gmail.com>
2024-02-18 14:23:04 +0500

    added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://127.0.0.1:8000/api";
-// export const API_BASE_URL = "https://api.nadeemakhtarandco.com/api";
+// export const API_BASE_URL = "http://127.0.0.1:8000/api";
+export const API_BASE_URL = "https://api.nadeemakhtarandco.com/api";
 export const MarketAddress = "0x9868Bc8528013CBc675b411bF56d48A2Ed670920";
 export const TokenAddress = "0x6bab579C6A8aBBb0BEf0ecE1b2F66cE12b9F07aD";
 
